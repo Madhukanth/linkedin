0adfa7bd6bd8e26b546002d241ba186e0833c839
Madhukanth T <madhu051196@gmail.com>
2018-07-22 22:20:40 +0530

    Final Commit

--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import queryString from 'query-string';
+import axios from 'axios';
 import './profile.css';
 
 export default class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      params: {}
+      params: {},
+      name: '',
+      nickname: '',
+      picture: '',
+      familyName: '',
+      givenName: ''
     };
   }
   async componentDidMount() {
@@ -14,17 +20,26 @@ export default class Profile extends Component {
       params: queryString.parse(this.props.location.search)
     });
     console.log(this.state.params);
+    await axios({
+      method: 'post',
+      url: 'https://secure-reaches-84828.herokuapp.com/auth/linkedin/fetchuser',
+      data: {
+        token: this.state.params
+      }
+    }).then(res => {
+      this.setState({ name: res.details.name });
+      this.setState({ nickname: res.details.nickname });
+      this.setState({ picture: res.details.picture });
+      this.setState({ familyName: res.details.family_name });
+      this.setState({ givenName: res.details.given_name });
+    });
   }
 
   render() {
     return (
       <div id="container">
-        <img
-          id="profile-img"
-          src={this.state.params.picture}
-          alt="Profile_Picture"
-        />
-        <h2 className="name">{this.state.params.name}</h2>
+        <img id="profile-img" src={this.state.picture} alt="Profile_Picture" />
+        <h2 className="name">{this.state.name}</h2>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/2000px-LinkedIn_Logo.svg.png"
           alt="Logo"
@@ -38,7 +53,7 @@ export default class Profile extends Component {
                 <h3 id="nickname">NickName:</h3>
               </td>
               <td>
-                <h3>{this.state.params.nickname}</h3>
+                <h3>{this.state.nickname}</h3>
               </td>
             </tr>
             <tr>
@@ -46,7 +61,7 @@ export default class Profile extends Component {
                 <h3 id="family-name">FamilyName:</h3>
               </td>
               <td>
-                <h3>{this.state.params.family_name}</h3>
+                <h3>{this.state.family_name}</h3>
               </td>
             </tr>
             <tr>
@@ -54,7 +69,7 @@ export default class Profile extends Component {
                 <h3 id="given-name">GivenName:</h3>
               </td>
               <td>
-                <h3>{this.state.params.given_name}</h3>
+                <h3>{this.state.given_name}</h3>
               </td>
             </tr>
           </tbody>
